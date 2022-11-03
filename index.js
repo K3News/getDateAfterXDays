32a3a274f386b1017eab387540f1ed7bf4822824
Fake Jobs Alert <102670391+K3News@users.noreply.github.com>
2022-11-03 15:44:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const addDays = require("date-fns/addDays");
 
 function getDate(x) {
-  const result = addDays(new Date(2020, 7, 22), parseInt(x));
+  const result = addDays(new Date(2020, 7, 22), parseInt(x));//trird only x also
   return result;
 }
 
